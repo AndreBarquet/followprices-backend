c0d4630b4857f826a7edc9cdcccda1d1d60eb5e6
Andre Barquet <andreluiz97.bm@gmail.com>
2022-04-20 14:08:34 -0300

    add get all saved setups

--- api/controllers/setups.js
+++ api/controllers/setups.js
@@ -1,4 +1,4 @@
-const { getOrder } = require('../../utils/queryUtils');
+const { getOrder, getPagination, getPagingData } = require('../../utils/queryUtils');
 const { Op } = require('@sequelize/core');
 const { exists, notExists } = require('../../utils/utils');
 
@@ -6,6 +6,27 @@ const { exists, notExists } = require('../../utils/utils');
 module.exports = app => {
   const { setupsTable, setupItemsTable } = require('../../models/dbTables');
   const controller = {};
+
+  // Get all setups list
+  controller.getAll = (req, res) => {
+    const where = { [Op.and]: [] };
+
+    if (exists(req.query.name)) where.name = { [Op.like]: `%${req.query.name}%` };
+    if (exists(req.query.description)) where.description = { [Op.like]: `%${req.query.description}%` };
+
+    // const typeFieldsToSelect = [{ model: typesTable, required: true, attributes: ["id", "description"] }]
+
+    // Oder field must be ASC or DESC
+    const order = getOrder(req.query.field, req.query.order);
+    const filters = { where, /* include: typeFieldsToSelect, */ order, ...getPagination(req.query.page, req.query.size) }
+
+    setupsTable.findAndCountAll(filters).then((response) => {
+      res.status(200).json(getPagingData(response, req.query.page, req.query.size));
+    }).catch(err => {
+      console.log("ERROR...:", err);
+      res.status(500).json({ error: 'Ocorreu um erro ao buscar os setups salvos' })
+    })
+  };
 
   // Insert new setup
   controller.insert = (req, res) => {
